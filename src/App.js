2035636c88eabfa5d6bd1d375eb445c6b496eae4
Akshat Rawat <akshat010501@gmail.com>
2022-05-09 01:58:24 +0530

    build AppDrawer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import AppDrawer from './components/AppDrawer'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={darkTheme}>
     <div className="App">
+      <AppDrawer />
       <header className="App-header">
         Samsung PRISM
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
